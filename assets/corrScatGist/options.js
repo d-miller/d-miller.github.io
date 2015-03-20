6b93b486717ddc23b94fa512c9664d00ab927065
d-miller <david.isaac.miller@gmail.com>
2015-03-20 04:47:18 -0500

    e

--- assets/corrScatGist/options.js
+++ assets/corrScatGist/options.js
@@ -6,12 +6,12 @@
 jsonFile = "http://d-miller.github.io/assets/corrScatGist/dataNew.json";
 
 //dimension of SVG
-var h = 450;
+var h = 300;
 var w = h;
-var pad = {left: 70, top: 60, right: 5, bottom: 105, middle: 120 };
+var pad = {left: 0, top: 0, right: 5, bottom: 0, middle: 90 };
 var corrInnerPad = 1;
 var scatInnerPad = 10;
-var radius = 5;
+var radius = 3;
 var highlightRadius = 9
 var brushRadius = 7;
 
@@ -43,7 +43,8 @@ var pos2 = { left: (pad.left + w + pad.middle) + "px",   top: (pad.top + h + 80)
 var pos3 = { left: (pad.left)                  + "px",   top: (pad.top + h + 22) + "px" };
 var pos4 = { left: (pad.left+50)               + "px",   top: (pad.top + h + 40) + "px" };
 var style0 = {color: "black"};
-var style1 = {color: "black", "font-size": "40px"};
+//var style1 = {color: "black", "font-size": "40px"};
+var style1 = {color: "black", "font-size": "20px"};
 var instrucsDatum = [
   {prevStep: 0, text: "Loading data, please wait...", trans: false,  delay: 0,     fadeOut: 0,    fadeIn: 0,     style: style0,  pos: pos0},
   {prevStep: 1, text: "Hover above to begin",         trans: false,  delay: 0,     fadeOut: 0,    fadeIn: 0,     style: style1,  pos: pos1},
