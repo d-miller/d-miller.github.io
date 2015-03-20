5f7f71154a9031698e0d7cc38bbf25e04af956d7
d-miller <david.isaac.miller@gmail.com>
2015-03-20 05:17:04 -0500

    r

--- assets/corrScatGist/options.js
+++ assets/corrScatGist/options.js
@@ -8,7 +8,7 @@ jsonFile = "http://d-miller.github.io/assets/corrScatGist/dataNew.json";
 //dimension of SVG
 var h = 300;
 var w = h;
-var pad = {left: 60, top: 60, right: 5, bottom: 105, middle: 120 };
+var pad = {left: 60, top: 60, right: 5, bottom: 60, middle: 90 };
 var corrInnerPad = 1;
 var scatInnerPad = 10;
 var radius = 4;
