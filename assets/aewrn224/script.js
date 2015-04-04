df7a997c23c47da45ebddeaddef44c8b7f0b93d3
d-miller <david.isaac.miller@gmail.com>
2015-04-04 15:15:19 -0500

    updated navbar

--- assets/aewrn224/script.js
+++ assets/aewrn224/script.js
@@ -1,10 +1,38 @@
 //jQuery for the navigation bar
 $(document).on("ready", function() {
+
+	//event handler for clicking a navigation link
     $(".nav a").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-  });
+	    $(".nav").find(".active").removeClass("active");
+	    $(this).parent().addClass("active");
+  	});
+
+	//get scrolll positions of HTML sections
+	var pos2 = $("#elite").position().top - 30;
+	var pos3 = $("#top25rank").position().top - 30;
+
+  	//jQuery for changing the active navigation link
+	$(document).scroll(function() {
+	
+		//current position
+		var curr = $(document).scrollTop();
+		var section = 1;
+		if (curr > pos2) section = 2;
+		if (curr > pos3) section = 3;
+
+		//change active class
+		$(".nav").find(".active").removeClass("active");
+		if (section==1) $(".nav").find("#link1").addClass("active");
+		if (section==2) $(".nav").find("#link2").addClass("active");
+		if (section==3) $(".nav").find("#link3").addClass("active");
+	});
 });
+
+
+
+
+
+
 
 //scale
 var scaleRB6 = ['rgb(178,24,43)','rgb(239,138,98)','rgb(253,219,199)','rgb(209,229,240)','rgb(103,169,207)','rgb(33,102,172)'];
