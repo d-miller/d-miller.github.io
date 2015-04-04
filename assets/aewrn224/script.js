9a7f8fae08f55fc03b7b00522e001720668e529d
d-miller <david.isaac.miller@gmail.com>
2015-04-04 15:44:49 -0500

    add smooth scrolling

--- assets/aewrn224/script.js
+++ assets/aewrn224/script.js
@@ -1,3 +1,21 @@
+
+//jQuery for smooth transitions
+//https://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
+
 //jQuery for the navigation bar
 $(document).on("ready", function() {
 
